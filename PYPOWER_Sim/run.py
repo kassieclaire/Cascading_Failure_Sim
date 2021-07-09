43228d83016a78a779cd3237a6461bca4ad5fb32
Jamir Shariar Jyoti <42971107+jamirshariar@users.noreply.github.com>
2021-07-09 15:46:16 -0500

    Update run.py

--- PYPOWER_Sim/run.py
+++ PYPOWER_Sim/run.py
@@ -6,5 +6,4 @@ initialFailures = 3
 loadGenerationRatio = .7
 loadShedConstant = .97
 capacityEstimationError = .02
-
 States=find_state_space(topology, iterations, initialFailures, loadGenerationRatio, loadShedConstant, capacityEstimationError);
