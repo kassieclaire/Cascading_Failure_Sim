a9c3074284bb0e569c5097b984b87df53c8b203b
kassie_claire <kassie.povinelli@gmail.com>
2021-08-04 08:59:40 -0500

    added mean and variance to the states dataframe in preparation for curve-fitting

---
+++ Mapping_T_input_to_T_output/README.txt
@@ -0,0 +1,4 @@
+This code is designed to map the mean and variance of an input T (topological variable)
+to an output mean and variance of a new T using the PMF generated
+
+Use run.py to run the code, and change the inputs to the function according to what is required
---
+++ Mapping_T_input_to_T_output/run.py
@@ -0,0 +1,4 @@
+from calculate_mean_and_variance import calculate_mean_and_variance
+#calculate_mean_and_variance(states_matrix, initial_failures)
+calculate_mean_and_variance(states_matrix='states', initial_failures='initial_failures', cluster_info = 'cluster_branch_118') #This uses the example fully-generated and bug-free IEEE118 states matrix and initial failures for mu and variance testing
+#map_T(number_of_lines, states_matrix, initial_failures)
