68c35b038c3696ac3e4c795df7e446e071e20d18
kassie_claire <kassie.povinelli@gmail.com>
2021-08-19 14:13:41 -0500

    mean_and_variance work

--- Mapping_T_input_to_T_output/normalize_pmf_keys.py
+++ Mapping_T_input_to_T_output/normalize_pmf_keys.py
@@ -1,10 +1,19 @@
-def normalize_pmf_keys(pmf, steady_state):
+def normalize_pmf_keys(pmf, steady_state, debug = False):
     normalized_pmf = {}
     normalized_steady_state_track = {}
     for key in pmf:
         list_key = list(key)
         normalized_key = tuple([float(i)/sum(list_key) for i in list_key])
-        normalized_pmf[normalized_key] = pmf[key]
-        normalized_steady_state_track[normalized_key] = steady_state[key]
+        if normalized_key not in normalized_pmf:
+            normalized_pmf[normalized_key] = pmf[key]
+            normalized_steady_state_track[normalized_key] = steady_state[key]
+            #For when keeping track of overlaps matters
+            #normalized_pmf[normalized_key] = [pmf[key]]
+            #normalized_steady_state_track[normalized_key] = [steady_state[key]]
+        else:
+            if debug == True:
+                print("key overlap!")
+            #normalized_pmf[normalized_key].append(pmf[key])
+            #normalized_steady_state_track[normalized_key].append(steady_state[key])
 
     return normalized_pmf, normalized_steady_state_track
--- Mapping_T_input_to_T_output/run.py
+++ Mapping_T_input_to_T_output/run.py
@@ -4,13 +4,17 @@ from generate_pmf import generate_pmf
 from trim_steady_regions import trim_steady_regions
 from cascading_failure_function import cascading_failure_function
 from normalize_pmf_keys import normalize_pmf_keys
+import pandas as pd
 df_name = 'test_df'
+mu_and_var_df_name = 'mean_and_variance.csv'
 cascading_failure_function(clusters_matrix_name='cluster_branch_39', output_df_name=df_name, use_simplified_df=True, states_matrix_name='case39_initial_failures_count_2_sm', initial_failure_table_name='case39_initial_failures_count_2_if', graph_pstop_simple=False)
 (region_failure_pmf, result_in_new_failure) = generate_pmf(df_name)
-(trimmed_region_failure_pmf, trimmed_steady_state_track) = trim_steady_regions(region_failure_pmf, result_in_new_failure, occurrence_floor=10)
-(normalized_pmf, normalized_steady_state_track) = normalize_pmf_keys(trimmed_region_failure_pmf, trimmed_steady_state_track)
+(trimmed_region_failure_pmf, trimmed_steady_state_track) = trim_steady_regions(region_failure_pmf, result_in_new_failure, occurrence_floor=5)
+(normalized_pmf, normalized_steady_state_track) = normalize_pmf_keys(trimmed_region_failure_pmf, trimmed_steady_state_track, debug=True)
 mu_and_var = calculate_mean_and_variance(normalized_pmf, normalized_steady_state_track)
 print(mu_and_var)
+#mu_and_var_df = pd.DataFrame.from_dict(mu_and_var)
+#mu_and_var_df.to_csv(mu_and_var_df_name, index=False)
 #print(trimmed_region_failure_pmf)
 #mu_and_var = calculate_mean_and_variance(trimmed_region_failure_pmf)
 
