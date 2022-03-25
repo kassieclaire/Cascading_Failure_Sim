8931bccf1e3c87ae42cfe61f8bc53bc0c3aeafb1
kassieclaire <59930829+kassieclaire@users.noreply.github.com>
2022-03-25 13:47:16 -0500

    updated scatterplot

--- Mapping_T_input_to_T_output/cmax_scatterplot.py
+++ Mapping_T_input_to_T_output/cmax_scatterplot.py
@@ -23,6 +23,7 @@ from scipy.stats import pearsonr
 #NOTE: Show dramatic event that happens, here's the outcome
 #NOTE: Ukraine cyberattacks powergrid blackout -- 2015
 
+normalize_across_F = False
 #number of points needed for graphing for particular F, mu, var combination
 cutoff = 5
 #counter for number of F, mu, var combinations that meet the cutoff
@@ -31,17 +32,13 @@ meets_cutoff_counter = 0
 df = pd.read_csv('mean_and_variance.csv')
 #find unique F values and iterate through these
 failure_vals = df["F"].unique()
-for f in failure_vals:
-    #get the df for only this failure value
-    separated_by_f_df = df[df['F'] == f]
-    #find unique input means for that F and iterate through these
-    mean_vals = separated_by_f_df['mu_i'].unique()
-    
-    #find unique input means for that F and iterate through these
-    mean_vals = separated_by_f_df['mu_i'].unique()
+if normalize_across_F:
+    print('Normalized across F')
+    #find unique input means, iterate through these
+    mean_vals = df['mu_i'].unique()
     for mu in mean_vals:
         #get the df for only this failure value and mean
-        separated_by_f_and_mu_df = separated_by_f_df[separated_by_f_df['mu_i'] == mu]
+        separated_by_f_and_mu_df = df[df['mu_i'] == mu]
         #find unique input variances for that F and mean and iterate through these
         var_vals = separated_by_f_and_mu_df['var_i'].unique()
         for var in var_vals:
@@ -77,7 +74,7 @@ for f in failure_vals:
                 fig = plt.figure()
                 ax1 = fig.add_subplot(111)
                 #create label names
-                title = 'Scatterplot showing pmf mean, variance clusters based on CMax for f = ' + str(f) + ", mean = " +  str(mu) + ", and variance = " + str(var)
+                title = 'Scatterplot showing pmf mean, variance clusters based on CMax for mean = ' +  str(mu) + ", \n and variance = " + str(var)
                 label_1 = 'Cmax less than or equal to ' + str(cmax_median)
                 label_2 = 'Cmax greater than ' + str(cmax_median)
                 #plot the points for less than
@@ -92,6 +89,69 @@ for f in failure_vals:
                 plt.xlabel('Mean of output PMF')
                 plt.ylabel('Variance of output PMF')
                 plt.show()               
-                
+            
+else:
+    for f in failure_vals:
+        #get the df for only this failure value
+        separated_by_f_df = df[df['F'] == f]
+        #find unique input means for that F and iterate through these
+        mean_vals = separated_by_f_df['mu_i'].unique()
+        
+        #find unique input means for that F and iterate through these
+        mean_vals = separated_by_f_df['mu_i'].unique()
+        for mu in mean_vals:
+            #get the df for only this failure value and mean
+            separated_by_f_and_mu_df = separated_by_f_df[separated_by_f_df['mu_i'] == mu]
+            #find unique input variances for that F and mean and iterate through these
+            var_vals = separated_by_f_and_mu_df['var_i'].unique()
+            for var in var_vals:
+                #get the df for only this failure value, mean, and variance
+                separated_by_f_mu_and_var_df = separated_by_f_and_mu_df[separated_by_f_and_mu_df['var_i'] == var]
+                #print the unique Cmax values
+                cmax_vals = separated_by_f_mu_and_var_df['Cmax'].unique()
+                if (len(cmax_vals) > cutoff):
+                    #sort the cmax vals in increasing order
+                    cmax_vals.sort()
+                    cmax_median = median(cmax_vals)
+                    #now, for each cmax value, get the mean of the pmf variances
+                    cmax_list = []
+                    mu_o_averaged_list_less = []
+                    mu_o_averaged_list_more = []
+                    var_o_averaged_list_less = []
+                    var_o_averaged_list_more = []
+                    #go through each cmax
+                    for cmax in cmax_vals:
+                        #append the cmax to a list to keep track of which cmax goes with the averaged var_o
+                        cmax_list.append(cmax)
+                        #get the df for only this failure value, mean, variance, and cmax
+                        separated_by_f_mu_var_and_cmax_df = separated_by_f_mu_and_var_df[separated_by_f_mu_and_var_df['Cmax'] == cmax]
+                        #append the averaged mean_o to the mean_o_averaged_list
+                        #choose which of the 2 lists to add the value to
+                        if cmax <= cmax_median:
+                            mu_o_averaged_list_less.append(mean(separated_by_f_mu_var_and_cmax_df['mu_o']))
+                            var_o_averaged_list_less.append(mean(separated_by_f_mu_var_and_cmax_df['var_o']))
+                        else:
+                            mu_o_averaged_list_more.append(mean(separated_by_f_mu_var_and_cmax_df['mu_o']))
+                            var_o_averaged_list_more.append(mean(separated_by_f_mu_var_and_cmax_df['var_o']))
+                    #graph the cmax versus the pmf variance averaged
+                    fig = plt.figure()
+                    ax1 = fig.add_subplot(111)
+                    #create label names
+                    title = 'Scatterplot showing pmf mean, variance clusters based on CMax for f = ' + str(f) + ", mean = " +  str(mu) + ", \n and variance = " + str(var)
+                    label_1 = 'Cmax less than or equal to ' + str(cmax_median)
+                    label_2 = 'Cmax greater than ' + str(cmax_median)
+                    #plot the points for less than
+                    ax1.scatter(mu_o_averaged_list_less, var_o_averaged_list_less, s=10, c='b', marker="s", label=label_1)
+                    #plot the points for more than
+                    ax1.scatter(mu_o_averaged_list_more,var_o_averaged_list_more, s=10, c='r', marker="o", label=label_2)
+                    #add legend
+                    plt.legend(loc='upper left')
+                    #add title
+                    plt.title(title)
+                    #add labels
+                    plt.xlabel('Mean of output PMF')
+                    plt.ylabel('Variance of output PMF')
+                    plt.show()               
+                    
 
                     
